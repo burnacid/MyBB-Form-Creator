ca40c8dc9f6b3a1b44578c4c48cb1cf8d0843d3d
Stefan Lenders <stefan@lenders-it.nl>
2017-05-17 09:56:37 +0200

    Added form.php language strings

--- inc/languages/english/formcreator.lang.php
+++ inc/languages/english/formcreator.lang.php
@@ -1,1 +1,12 @@
+<?php
 
+$l['formcreator'] = "Form Creator";
+$l['fc_is_empty'] = "is empty!";
+$l['fc_no_match_regex'] = "did not match the expected input!";
+$l['fc_submitted'] = "Form is submitted";
+$l['fc_no_fields'] = "This form doesn't contain any fields yet!";
+$l['fc_form_disabled'] = "This form has been disabled for use!";
+$l['fc_form_no_permissions'] = "You are not allowed to use this form!";
+$l['fc_no_form_found'] = "The form you are looking for doesn't exist!";
+
+?>
