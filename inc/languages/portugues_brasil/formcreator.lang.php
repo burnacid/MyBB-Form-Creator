841e1424d760f122b24ee690dd5d561913899cf9
Stefan Lenders <stefan@lenders-it.nl>
2017-10-27 18:29:00 +0200

    #61 Add Portuguese-Brazil Translation
    By @Walter-Correa

---
+++ inc/languages/portugues_brasil/formcreator.lang.php
@@ -0,0 +1,16 @@
+<?php
+
+// Author: Walter_Correa
+// Version: 2.1.0
+
+$l['formcreator'] = "Criador de Formulário";
+$l['fc_is_empty'] = "está vazio!";
+$l['fc_no_attachment'] = "nenhum anexo foi enviado!";
+$l['fc_no_match_regex'] = "não coincide com o esperado!";
+$l['fc_submitted'] = "O formulário foi enviado!";
+$l['fc_no_fields'] = "Este formulário ainda não contém nenhum Campo!";
+$l['fc_form_disabled'] = "Este formulário foi desativado e não pode ser usado!";
+$l['fc_form_no_permissions'] = "Você não está permitido a usar este formulário!";
+$l['fc_no_form_found'] = "O formulário que você está buscando não existe!";
+
+?>
