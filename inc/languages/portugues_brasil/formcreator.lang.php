e238bc035901549e895d985ddc08726f4d6f9651
Stefan Lenders <stefan@lenders-it.nl>
2017-10-29 12:12:31 +0000

    Translated using Weblate (portugues_BRASIL (generated))
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: MyBB Form Creator/Language Pack
    Translate-URL: http://translate.lenders-it.nl/projects/mybb-form-creator/language-pack/portugues_BRASIL/

--- inc/languages/portugues_brasil/formcreator.lang.php
+++ inc/languages/portugues_brasil/formcreator.lang.php
@@ -2,9 +2,9 @@
 
 $l['formcreator'] = "Criador de Formulário";
 $l['fc_is_empty'] = "está vazio!";
-$l['fc_no_attachment'] = "nenhum anexo foi enviado!";
+$l['fc_no_attachment'] = "Nenhum anexo foi enviado";
 $l['fc_no_match_regex'] = "não coincide com o esperado!";
-$l['fc_submitted'] = "O formulário foi enviado!";
+$l['fc_submitted'] = "O formulário foi enviado";
 $l['fc_no_fields'] = "Este formulário ainda não contém nenhum Campo!";
 $l['fc_form_disabled'] = "Este formulário foi desativado e não pode ser usado!";
 $l['fc_form_no_permissions'] = "Você não está permitido a usar este formulário!";
