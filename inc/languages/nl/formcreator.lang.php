9494a766e79ee59966d0d7d13ac6821ffaeb3111
Stefan Lenders <stefan@lenders-it.nl>
2017-11-03 13:51:45 +0000

    Added translation using Weblate (Dutch)

---
+++ inc/languages/nl/formcreator.lang.php
@@ -0,0 +1,14 @@
+<?php
+$l['formcreator'] = "Form Creator";
+$l['fc_is_empty'] = "is empty!";
+$l['fc_no_attachment'] = "No attachment was uploaded";
+$l['fc_no_match_regex'] = "did not match the expected input!";
+$l['fc_submitted'] = "Form is submitted";
+$l['fc_no_fields'] = "This form doesn't contain any fields yet!";
+$l['fc_form_disabled'] = "This form has been disabled for use!";
+$l['fc_form_no_permissions'] = "You are not allowed to use this form!";
+$l['fc_no_form_found'] = "The form you are looking for doesn't exist!";
+$l['fc_access_denied'] = "Access Denied";
+$l['fc_form_disabled_title'] = "Form Disabled";
+$l['fc_limit_reached_title'] = "Form Limit Reached";
+$l['fc_limit_reached'] = "You have reached the maximum form usage";
