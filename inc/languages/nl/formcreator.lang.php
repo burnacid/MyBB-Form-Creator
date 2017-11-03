cb8de08fc64fca6a959be4029c8f548dfa47df31
Stefan Lenders <stefan@lenders-it.nl>
2017-11-03 14:00:58 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: MyBB Form Creator/Language Pack
    Translate-URL: http://translate.lenders-it.nl/projects/mybb-form-creator/language-pack/nl/

--- inc/languages/nl/formcreator.lang.php
+++ inc/languages/nl/formcreator.lang.php
@@ -1,5 +1,5 @@
 <?php
-$l['formcreator'] = "Formulier Generator";
+$l['formcreator'] = "Form Creator";
 $l['fc_is_empty'] = "is leeg!";
 $l['fc_no_attachment'] = "Er is geen bijlage geupload";
 $l['fc_no_match_regex'] = "komt niet overeen met de verwachte input!";
@@ -12,3 +12,4 @@ $l['fc_access_denied'] = "Toegang Geweigerd";
 $l['fc_form_disabled_title'] = "Formulier Uitgeschakeld";
 $l['fc_limit_reached_title'] = "Formulier Limiet Bereikt";
 $l['fc_limit_reached'] = "U heeft het maximaal aantal gebruiken bereikt";
+$l['fc_test_new_line'] = "Dit is een test";
